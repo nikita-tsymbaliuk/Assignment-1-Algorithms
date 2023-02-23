cfc58d3516b1266e3f093f036b3845b94b881e01
Kristina <kristinaer304@gmail.com>
2023-02-23 15:29:04 +0200

    Metods: Delimeter Operator Priority

--- Solution.cs
+++ Solution.cs
@@ -61,6 +61,40 @@ namespace ConsoleApp7
         }
 
     }
-    
+    class Rpn
+    {
+        //Метод повертає true, якщо символ, що перевіряється - роздільник ("пробіл")
+        static private bool isDelimeter(char c)
+        {
+            string probel = " ";
+            if (probel.IndexOf(c) != -1)
+                return true;
+            return false;
+        }
+
+        //Метод повертає true, якщо символ, що перевіряється - оператор
+        static private bool isOperator(char с)
+        {
+            string templateOperators = "+-/*^()";
+            if (templateOperators.IndexOf(с) != -1)
+                return true;
+            return false;
+        }
+
+        //Метод повертає пріоритет оператора
+        static private byte priorityOfOperators(char symbol)
+        {
+            switch (symbol)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 3;
+                case '*': return 4;
+                case '/': return 4;
+                case '^': return 5;
+                default: return 6;
+            }
+        }    
     
     
